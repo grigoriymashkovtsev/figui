6cc49370f006def16a0b52c3e952d7d1cc113b71
Grigoriy <grigoriymashkovtsev@gmail.com>
2024-01-27 13:57:17 +0300

    add theming

--- dev/App.svelte
+++ dev/App.svelte
@@ -1,6 +1,32 @@
 <script lang="ts">
 import Input from '../src/components/Input/Input.svelte';
+
+let selected = 'light';
+
+// @ts-ignore
+function onChange(event) {
+    selected = event.currentTarget.value;
+}
 </script>
+
+<svelte:head>
+    <!-- <base href="/" /> -->
+    {#if selected === 'light'}
+        <link rel="stylesheet" href="./css/light.css" />
+    {:else}
+        <link rel="stylesheet" href="./css/dark.css" />
+    {/if}
+</svelte:head>
+
+<div>
+    <label>
+        <input checked={selected === 'light'} on:change={onChange} type="radio" name="amount" value="light" /> Light
+    </label>
+
+    <label>
+        <input checked={selected === 'dark'} on:change={onChange} type="radio" name="amount" value="dark" /> Dark
+    </label>
+</div>
 
 <main>
     <Input label={'hello!'}></Input>
--- dev/main.ts
+++ dev/main.ts
@@ -1,12 +1,12 @@
 import App from './App.svelte';
 
-let theme: 'light' | 'dark' = 'light';
+// let theme: 'light' | 'dark' = 'light';
 
-if (theme === 'light') {
-    import('./light.css');
-} else {
-    import('./dark.css');
-}
+// if (theme === 'light') {
+//     import('./light.css');
+// } else {
+//     import('./dark.css');
+// }
 import './app.css';
 
 const app = new App({
